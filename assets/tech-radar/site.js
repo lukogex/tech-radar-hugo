86af136541bcb725e344c109f009bf1d69ff37dc
Lukas Kranabetter <l.kranabetter@netconomy.net>
2024-07-04 00:20:15 +0200

    feat(WUERTH-1780): Store progress

--- assets/tech-radar/site.js
+++ assets/tech-radar/site.js
@@ -1,3 +1,4 @@
+import * as params from '@params';
 import { Buffer } from 'buffer';
 
 const CSVBuilder = require('./util/factory');
@@ -6,5 +7,5 @@ window.process = require('process');
 window.global = window;
 window.Buffer = Buffer;
 
-var builder = CSVBuilder('http://localhost:1313/tech-radar/data/index.csv');
+var builder = CSVBuilder(params.baseUrl + '/tech-radar/data/index.csv');
 builder.init().build();
